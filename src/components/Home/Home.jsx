ef50e44ad712bdc0d7bbf860610f90bee6409077
chandan shutradhar <145096947+chandandev72@users.noreply.github.com>
2023-12-20 21:14:09 -0800

    as

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../shard/Header/Header';
 import Banner from './Banner/Banner';
+import Services from './Services/Services';
+import Gallery from './Gallery/Gallery';
 const Home = () => {
     return (
      <div>
 
       <Header></Header>
       <Banner></Banner>
+      <Services></Services>
+      <Gallery></Gallery>
     
      </div>
 
@@ -14,3 +18,4 @@ const Home = () => {
 };
 
 export default Home;
+
